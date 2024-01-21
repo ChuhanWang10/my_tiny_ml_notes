7e8ffdb6b11bc263f9f0291bd620b618449cedd8
chuhan <chuhanwang10@outlook.com>
2024-01-21 23:43:04 +0100

    add files

---
+++ utils.py
@@ -0,0 +1,32 @@
+from torchprofile import profile_macs
+
+
+# download checkpoints 
+def download_url(url, model_dir='.', overwrite=False):
+    import os, sys, ssl
+    from urllib.request import urlretrieve
+    ssl._create_default_https_context = ssl._create_unverified_context
+    target_dir = url.split('/')[-1]
+    model_dir = os.path.expanduser(model_dir)
+    try:
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        model_dir = os.path.join(model_dir, target_dir)
+        cached_file = model_dir
+        if not os.path.exists(cached_file) or overwrite:
+            sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
+            urlretrieve(url, cached_file)
+        return cached_file
+    except Exception as e:
+        # remove lock file so download can be executed next time.
+        os.remove(os.path.join(model_dir, 'download.lock'))
+        sys.stderr.write('Failed to download from url %s' % url + '\n' + str(e) + '\n')
+        return None
+
+
+# Compute MACs
+def get_model_macs(model, inputs) -> int:
+    from torchprofile import profile_macs
+    return profile_macs(model, inputs)
+
+
